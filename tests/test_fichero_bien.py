5584f229152f4609fd3f13856a2ca4d82614a7cf
Marc Masip <mmasipcoloma@gmail.com>
2021-01-06 19:07:47 +0100

    Config commit, test don't work

--- tests/test_fichero_bien.py
+++ tests/test_fichero_bien.py
@@ -36,9 +36,19 @@ class TestFicheroBien(unittest.TestCase):
 
         self.assertEqual(tfg_nlp.remove_multiple_white_spaces(actual), expected)
 
-    def test_splitter_punctuation_marks(self):
-        actual = ["hola - como - estas / que haces; aqui"]
-        expected = ["hola", "como", "estas", "que haces", "aqui"]
+    def test_punctuation_mark_coma(self):
+        row1 = ['https://vivo.invid.udl.cat/individual/UDL-00000467',
+                "colonic metabolism; microbiome; polyphenols;phenol metabolism; ultra-performance liquid chromatography mass spectrometry; wine"]
+
+        row2 = [["https://vivo.invid.udl.cat/individual/UDL-00000467","colonic metabolism"],
+                ["https://vivo.invid.udl.cat/individual/UDL-00000467", "microbiome"],
+                ["https://vivo.invid.udl.cat/individual/UDL-00000467", "polyphenols"],
+                ["https://vivo.invid.udl.cat/individual/UDL-00000467", "phenol metabolism"],
+                ["https://vivo.invid.udl.cat/individual/UDL-00000467", "ultra-performance liquid chromatography mass spectrometry"],
+                ["https://vivo.invid.udl.cat/individual/UDL-00000467", "wine"]]
+
+        actual = pd.DataFrame(row1, columns=['resource', 'keyword'])
+        expected = pd.DataFrame(row2, columns=['resource', 'keyword'])
 
         self.assertEqual(tfg_nlp.splitter(actual), expected)
 
